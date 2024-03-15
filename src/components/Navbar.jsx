eff53987bd351b0e18eef98d730a8336417f206c
Jatin <datajatin90@gmail.com>
2024-03-15 08:18:51 +0530

    First version

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+// import {FaBars , FaTimes} from "react-icons/fa"
+const Navbar = () => {
+    // links=[
+    //     {
+    //         id:1,
+    //         link:"/",
+    //         linkname:"home"
+    //     },
+    //     {
+    //         id:2,
+    //         link:"/",
+    //         linkname:"about"
+    //     },
+    //     {
+    //         id:3,
+    //         link:"/",
+    //         linkname:"services"
+    //     },
+    // ];
+  return (
+    <div className='flex justify-between text-4xl font-bold font-signatureFont'>
+        Jatin
+    </div>
+  )
+}
+
+export default Navbar
